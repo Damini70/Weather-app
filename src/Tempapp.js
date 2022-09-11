6e05df1e26bb1e8231c184feb8cc1b48c7bb9d71
Damini Raj <daminiraj70@gmail.com>
2022-09-11 13:39:31 +0530

    weather

--- src/Tempapp.js
+++ src/Tempapp.js
@@ -2,10 +2,14 @@ import React,{useState,useEffect} from "react";
 import {WiSunrise} from "react-icons/wi";
 import {WiHail} from "react-icons/wi";
 import {WiSunset} from "react-icons/wi";
+import {WiDayHaze} from "react-icons/wi";
 import { FaTemperatureHigh } from "react-icons/fa";
 import { FaStreetView } from "react-icons/fa";
-
-import {WiRaindrop } from "react-icons/wi";
+import {WiShowers} from "react-icons/wi";
+import {WiCloudy} from "react-icons/wi";
+import {WiHumidity } from "react-icons/wi";
+import {WiStrongWind} from "react-icons/wi";
+import {WiTime3} from "react-icons/wi";
 
 
 import './App.css';
@@ -14,7 +18,10 @@ const Tempapp=()=>{
     const [city,setCity]=useState("");
     const [search,setSearch]=useState("Patna");
     const [sun,setSun]=useState({});
-    // const [sunset,setSunset]=useState("");
+    const [weather,setWeather]=useState("");
+    const [icon,setIcon]=useState("");
+    const [wind,setWind]=useState("");
+    const [time,setTime]=useState("");
 
     useEffect(()=>{
         const fetchApi=async()=>{
@@ -31,36 +38,79 @@ const Tempapp=()=>{
             const settime=`${set.getHours()}:${set.getMinutes()}`
             const risetime=`${rise.getHours()}:${rise.getMinutes()}`
             const data1={settime,risetime,country}
-            setSun(data1)
+            setSun(data1);
+            const {main,description}=resJson.weather[0]
+            // console.log(main);
+            const wea={main,description};
+            setWeather(wea);
+            const {speed}=resJson.wind;
+            setWind(speed);
+            setTime(new Date().toLocaleDateString());
+            console.log(time)
 
+            
+            
             
         
         };
         fetchApi();
     },[search])
+    useEffect(()=>{
+        if(weather){
+            
+            switch (weather.main){
+                case "Haze": setIcon(<WiDayHaze/>);
+                break;
+                case "Clear": setIcon();
+                break;
+                case "Clouds": setIcon(<WiCloudy/>);
+                break;
+                case "Rain":setIcon(<WiShowers/>)
+                break;
+                case "Clear": setIcon("")
+
+
+            }
+        }
+
+    },[weather])
+
+
     
    
     return(
         <>
         
-        <input type="search"  id="input" placeholder="Type your City"  value={search} onChange={(e)=>{setSearch(e.target.value)}} />
+        <input type="search"  id="input" placeholder="Type your City" auto="off" value={search} onChange={(e)=>{setSearch(e.target.value)}} />
         
        <br/><br/>
         <h1><FaStreetView style={{fontSize: "4rem"}}/> {search}</h1>
         {!city?(<h2>No City Found</h2>):
         (<div>
            <h3>Country : {sun.country}</h3> 
+           <h3>{icon} Weather : {weather.main}({weather.description})</h3>
            <h3 style={{color: "orange"}}>Temp : {city.temp} Celsius</h3> 
+
+           <h4><WiStrongWind/> Wind : {wind} m/s</h4>
+
           <div className="temp">
           <h3 style={{marginRight:"5rem"}}>Min Temp: {city.temp_min}  <FaTemperatureHigh style={{color: "skyblue"}}/></h3>
            <h3>Max Temp: {city.temp_max}  <FaTemperatureHigh style={{color: "red"}}/></h3>
-          </div>
-           <div className="extra">
-            <h3 style={{color: "aqua",marginRight:"5rem"}}><WiRaindrop style={{fontSize: "2rem"}}/>  Humidity: {city.humidity}%</h3>
-           <h3 style={{color: "aqua"}}><WiHail style={{fontSize: "2rem"}}/> Pressure: {city.pressure} hPa</h3>
-           </div>
+          </div><hr/>
 
-          <h4><span style={{paddingRight: "3rem"}}><WiSunrise style={{fontSize: "1.3rem",color: "orange"}}/> Sunrise: {sun.risetime}</span> <span><WiSunset style={{fontSize: "1.3rem",color: "darkgrey"}}/> Sunset: {sun.settime} </span>  </h4> 
+           <div className="extra">
+            <h3 style={{color: "aqua",marginRight:"5rem"}}><WiHumidity style={{fontSize: "2rem"}}/>  Humidity: {city.humidity}%</h3>
+           <h3 style={{color: "aqua"}}><WiHail style={{fontSize: "2rem"}}/> Pressure: {city.pressure} hPa</h3>
+           </div><hr/>
+         <div className="time">
+         <h4><span ><WiSunrise style={{fontSize: "1.9rem",color: "red"}}/> Sunrise: {sun.risetime}</span> <span><WiSunset style={{fontSize: "1.9rem",color: "darkorange"}}/> Sunset: {sun.settime} </span> 
+          <span><WiTime3 style={{fontSize: "1.9rem",color: "pink"}}/>Day: {time}</span> </h4> 
+         </div>
+        
+
+
+
+         
 
 
          
